79812bdf53c8aedd40d64d7f6199c0a254c9b10e
longgtruong <449360@student.saxion.nl>
2022-02-03 05:29:05 +0100

    add spotify chart

--- pages/charts/[country].tsx
+++ pages/charts/[country].tsx
@@ -1,35 +1,78 @@
 import axios from 'axios'
 import { GetServerSideProps } from 'next'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { Chart } from '../../src/components/Chart'
+import { COUNTRIES } from '../../src/constant/country'
+
+const HOST = "https://chart-data-tools.vercel.app/"
+// const HOST = "http://localhost:8080/"
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const { data: apple } = await axios.get(`https://chart-data-tools.vercel.app/apple-music/songs/${ctx.params?.country}`)
-    const { data: itunes } = await axios.get(`https://chart-data-tools.vercel.app/itunes/songs/${ctx.params?.country}`)
+    const { data: apple } = await axios.get(`${HOST}${ctx.params?.country}`)
+    const { data: itunes } = await axios.get(`${HOST}${ctx.params?.country}`)
+    const { data: spotify } = await axios.get(`${HOST}${ctx.params?.country}`)
     return {
         props: {
             apple,
-            itunes
+            itunes,
+            spotify
         }
     }
 }
 
-function Charts({ apple, itunes }: any) {
+function Charts({ apple, itunes, spotify }: any) {
+
+    const getInitialState = () => {
+        const value = "us"
+        return value;
+      };
+    
+      const [value, setValue] = useState(getInitialState)
+    
+      const handleChange = (e: any) => {
+        setValue(e.target.value)
+      }
+    
 
     const { query } = useRouter();
-    console.log(query.country)
-    console.log(apple)
-
+    console.log(spotify)
     return (
-        <div className="mt-6 flex max-w-8xl flex-wrap justify-around sm:w-full">
-            <div className="mt-6 w-98">
-                <h1 className="text-xl text-white font-bold">Apple Music {query.country?.toString().toUpperCase()}</h1>
-                <Chart data={apple}/>
+        <div className="content">
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                <h1 className="text-6xl font-bold text-white mb-5">
+                    Charts around the world
+        </h1>
+                <select value={value} onChange={handleChange}>
+                    {COUNTRIES.map((el, i) => (
+                        <option value={el.code} key={i}>{el.name}</option>
+                    ))}
+                </select>
+                <Link href={`/charts/${value}`}>
+                    <a className="bg-sky-500 mt-5 py-2 px-5 rounded-full text-white">
+                        <h1 className="text-m font-bold text-white">
+                            Get charts
+            </h1>
+                    </a>
+                </Link>
+            </main>
+            <div className="charts flex flex-wrap lg:m-10 sm:m-5">
+                <div className="mt-6 w-92 flex-1 m-5">
+                    <h1 className="text-xl text-white font-bold">Apple Music {query.country?.toString().toUpperCase()}</h1>
+                    <Chart data={apple} />
+                </div>
+                <div className="mt-6 w-92 flex-1 m-5">
+                    <h1 className="text-xl text-white font-bold">Spotify {query.country?.toString().toUpperCase()}</h1>
+                    <Chart data={spotify} />
+                </div>
+                <div className="mt-6 w-92 flex-1 m-5">
+                    <h1 className="text-xl text-white font-bold">iTunes {query.country?.toString().toUpperCase()}</h1>
+                    <Chart data={itunes} />
+                </div>
             </div>
-            <div className="mt-6 w-98">
-                <h1 className="text-xl text-white font-bold">iTunes {query.country?.toString().toUpperCase()}</h1>
-                <Chart data={itunes}/>
-            </div>
+
+
         </div>
     )
 }
--- src/components/ChartItem.tsx
+++ src/components/ChartItem.tsx
@@ -9,19 +9,26 @@ export const ChartItem = ({ chartObject }: Props) => {
     const artist = chartObject["Artist"]
     const link = chartObject["Link"]
     const image = chartObject["Image"]
+    const streams = chartObject["Streams"]
 
     return (
         <a href={link}>
-        <div className="chart-item text-left text-sky-500 flex mt-2 bg-slate-100 hover:bg-sky-400/50 hover:text-white duration-200 ease-in-out p-2 rounded-md items-center">
-            <div className="w-16 h-16">
-                <img className="rounded-full shadow-sm" src={image} />
+            <div className="chart-item text-left text-sky-500 flex mt-2 bg-slate-100 hover:bg-sky-400/50 hover:text-white duration-200 ease-in-out p-2 rounded-md items-center">
+                <div className="w-16 h-16">
+                    <img className="rounded-full shadow-sm" src={image} />
+                </div>
+                <div className="text-xl font-bold my-2 mx-4">{rank}</div>
+                {streams ? <div className="title ml-2">
+                    <div className="text-l font-bold info">{title}</div>
+                    <div className="text-l text-ellipsis overflow-hidden info">{artist}</div>
+                    <div className="text-l font-bold info">{streams} streams</div>
+                </div>
+                    : <div className="title ml-2 lg:my-3">
+                        <div className="text-l font-bold info">{title}</div>
+                        <div className="text-l text-ellipsis overflow-hidden info">{artist}</div>
+                    </div>
+                }
             </div>
-            <div className="text-l font-bold m-2">{rank}</div>
-            <div className="title ml-2">
-                <div className="text-l font-bold info">{title}</div>
-                <div className="text-l text-ellipsis overflow-hidden info">{artist}</div>
-            </div>
-        </div>
         </a>
     )
 }
